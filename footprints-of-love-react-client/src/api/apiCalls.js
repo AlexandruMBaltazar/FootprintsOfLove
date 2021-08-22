d6e7a65974ed2233cd72dfd6287ebf13fd84c0dd
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-08-22 18:12:20 +0100

    Add login functionality

--- footprints-of-love-react-client/src/api/apiCalls.js
+++ footprints-of-love-react-client/src/api/apiCalls.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const signup = (user) => {
   return axios.post("/api/register", user);
 };
+
+export const login = (user) => {
+  return axios.post("/api/login", user);
+};
--- footprints-of-love-react-client/src/pages/LoginPage.js
+++ footprints-of-love-react-client/src/pages/LoginPage.js
@@ -1,34 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Input from "../components/Input";
+import ButtonWithProgress from "../components/ButtonWithProgress";
+import * as apiCalls from "../api/apiCalls";
 
-const LoginPage = () => {
+const LoginPage = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState();
+  const [pendingApiCall, setPendingApiCall] = useState(false);
+
+  useEffect(() => {
+    setErrors();
+  }, [email, password]);
+
+  const submit = () => {
+    const user = {
+      email,
+      password,
+    };
+
+    setPendingApiCall(true);
+
+    apiCalls
+      .login(user)
+      .then((response) => {
+        setPendingApiCall(false);
+        props.history.push("/");
+      })
+      .catch((apiErrors) => {
+        setPendingApiCall(false);
+        setErrors(apiErrors.response.data.error);
+      });
+  };
+
+  let disableSubmit = email && password ? false : true;
+
   return (
     <div>
       <form className="d-flex flex-column w-50 offset-4 mt-5">
         <h1 className="h3 mb-3 fw-normal text-center">Log in</h1>
 
-        <div className="form-floating mb-1">
-          <input
+        <div className="mb-1">
+          <Input
             type="email"
-            className="form-control"
-            id="floatingInput"
+            name="email"
             placeholder="name@example.com"
-            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required={true}
+            label="Email address"
           />
-          <label for="floatingInput">Email address</label>
         </div>
-        <div className="form-floating">
-          <input
+        <div>
+          <Input
             type="password"
-            className="form-control"
-            id="floatingPassword"
+            name="password"
             placeholder="Password"
-            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required={true}
+            label="Password"
           />
-          <label for="floatingPassword">Password</label>
         </div>
-        <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">
-          Login
-        </button>
+        {errors && (
+          <div className="col-12 mt-1 mb-0 ">
+            <div className="alert alert-danger">{errors}</div>
+          </div>
+        )}
+        <ButtonWithProgress
+          className="w-100 btn btn-lg btn-primary mt-2"
+          onClick={submit}
+          disabled={disableSubmit || pendingApiCall}
+          pendingApiCall={pendingApiCall}
+          text="Sign Up"
+        />
         <p className="mt-5 mb-3 text-muted">&copy; Footprints of Love</p>
       </form>
     </div>

9e01d673da3f7e0592cfd37ea10821638bef55c5
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2022-01-10 17:00:13 +0000

    Endpoint to fetch liked and disliked users and users that likes you using scopes

--- footprints-of-love/app/Http/Controllers/LikesController.php
+++ footprints-of-love/app/Http/Controllers/LikesController.php
@@ -3,47 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\User\ShowResource;
-use App\Models\Swipe;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Support\Facades\Auth;
-
 
 class LikesController extends Controller
 {
-    public function whoLikesYou(Request $request): AnonymousResourceCollection
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return AnonymousResourceCollection
+     */
+    public function __invoke(Request $request): AnonymousResourceCollection
     {
-        $users = User::query()->whereHas('swipes', function ($query) {
-            return $query
-                ->doesntHave('match')
-                ->where('target_user_id', Auth::id())
-                ->where('liked', true);
-        })->simplePaginate(5);
-
-        return ShowResource::collection($users);
-    }
-
-    public function likedUsers(Request $request): AnonymousResourceCollection
-    {
-       $users = User::query()->whereHas('targetSwipes', function ($query) {
-            return $query
-                ->doesntHave('match')
-                ->where('user_id', Auth::id())
-                ->where('liked', true);
-        })->simplePaginate(5);
-
-        return ShowResource::collection($users);
-    }
-
-    public function dislikedUsers(Request $request): AnonymousResourceCollection
-    {
-        $users = User::query()->whereHas('targetSwipes', function ($query) {
-            return $query
-                ->doesntHave('match')
-                ->where('user_id', Auth::id())
-                ->where('liked', false);
-        })->simplePaginate(5);
+        $users = User::likes($request->type)->simplePaginate(5);
 
         return ShowResource::collection($users);
     }
